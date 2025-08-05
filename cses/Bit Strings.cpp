87ccd00aa0fd1a966ba60219c472c1f21e9b82f1
Shourya Kashyap <shourya.kashyap.05@gmail.com>
2025-08-05 19:20:57 +0530

    Update Bit Strings.cpp

--- cses/Bit Strings.cpp
+++ cses/Bit Strings.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-	int n; cin >> n;
-	const int N=1e9+7;
-	long long ans=2;
+    int n; cin >> n;
+    const int N=1e9+7;
+    long long ans=2;
     for(int i=1; i<n; i++) {
         ans*=2;
         ans=ans%N;
